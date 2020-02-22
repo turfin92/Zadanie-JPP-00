aebbe784a3c34ceb0775d6cd860c21fddbd7367d
uzytkownik <uzytkownik1@kursGIT.pl>
2020-02-22 14:42:09 +0100

    Dodano dane ze wzrostem

--- src/DaneWzrost.java
+++ src/DaneWzrost.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DaneWzrost {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("wpisz swoje imie");
+        String imie = scanner.nextLine();
+        System.out.println("Twoje imie to:" + imie);
+        System.out.println("wpisz swoje nazwisko");
+        String nazwisko = scanner.nextLine();
+        System.out.println("Twoje nazwisko :" + nazwisko);
+        System.out.println("wpisz swoj wiek");
+        int wiek = scanner.nextInt();
+        System.out.println("Twoj wiek to:" + wiek);
+        System.out.println("wpisz swoj wzrost w cm");
+        double wzrost = scanner.nextInt();
+        System.out.println("Twoj wzrost w metrach to:" + wzrost);
+        double wzrostwm= wzrost / 100;
+        System.out.println(wzrostwm);
+    }
 }
